7dee7d548b7eccfc1669d1c084bc2c106f01e0cc
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2022-02-14 14:01:14 +0900

    [ADD] SwiftUI Project 추가

---
+++ SwiftUI-Tutorials/SwiftUI-Tutorials/SwiftUI_TutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TutorialsApp.swift
+//  SwiftUI-Tutorials
+//
+//  Created by SHIN YOON AH on 2022/02/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
