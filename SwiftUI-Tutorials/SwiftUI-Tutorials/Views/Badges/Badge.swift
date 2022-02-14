eb3429e44ecd5067242cab2d303d38eba2176c7b
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2022-02-15 00:22:00 +0900

    [ADD] Drawing Paths and Shapes 튜토리얼 완료

---
+++ SwiftUI-Tutorials/SwiftUI-Tutorials/Views/Badges/Badge.swift
@@ -0,0 +1,39 @@
+//
+//  Badge.swift
+//  SwiftUI-Tutorials
+//
+//  Created by SHIN YOON AH on 2022/02/15.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var badgeSymbols: some View {
+        ForEach(0..<8) { index in
+            RotatedBadgeSymbol(
+                angle: .degrees(Double(index) / Double(8)) * 360.0
+            )
+        }
+        .opacity(0.5)
+    }
+    
+    var body: some View {
+        ZStack {
+            BadgeBackground()
+            
+            GeometryReader { geometry in
+                badgeSymbols
+                    .scaleEffect(1.0 / 4.0, anchor: .top)
+                    .position(x: geometry.size.width / 2.0, y: (3.0 / 4.0) * geometry.size.height)
+                
+            }
+        }
+        .scaledToFit()
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
---
+++ SwiftUI-Tutorials/SwiftUI-Tutorials/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  SwiftUI-Tutorials
+//
+//  Created by SHIN YOON AH on 2022/02/15.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
